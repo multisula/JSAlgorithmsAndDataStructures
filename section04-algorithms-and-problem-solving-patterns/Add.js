39b4c78d5b75a98e18bd0987389f3659aeea5d9f
sulad <sulad@localhost.localdomain>
2023-04-06 11:24:29 +0900

    add

---
+++ section04-algorithms-and-problem-solving-patterns/Add.js
@@ -0,0 +1,11 @@
+// Write a function which takes two numbers and returns their sum.
+
+// 1. Can I restate the problem in my own words?
+"implement addition"
+// 2. What are the inputs that go into the problem?
+  // int?, float?, string?
+// 3. What are the outputs that should come from the solution to the problem?
+  // int?, float?, string?
+// 4. Can the outputs be determined from the inputs?
+
+// 5. How sould I label the imporatant peices of data that are a part of the problem
