eb0a1805543ad8dbcdc1bbb5715a32baeff8aa21
sulad <sulad@localhost.localdomain>
2023-04-06 19:04:05 +0900

    refactor

--- section04-algorithms-and-problem-solving-patterns/CharCount.js
+++ section04-algorithms-and-problem-solving-patterns/CharCount.js
@@ -7,20 +7,29 @@ function charCount(str){
   // make object to return at end
   let result = {}
   // loop over string, for each character...
-  for(let i = 0; i < str.length; i++) {
-    let char = str[i].toLowerCase();
-    // if the char is a number/letter AND is a key in object, add one to count
-    if(result[char] > 0) {
-      result[char]++
-    }
-    // if the char is a number/letter AND not in object, add it to object and set value to 1
-    else {
-      result[char] = 1
-    }
+  // let regex = new RegExp('')
+  for (let char of str) {
+    // char = char.toLowerCase();
     // if character is something else (space, period, etc.) don't do anything
+    // if(/[a-z0-9]/.test(char)) {
+    if(isAlphaNumeric(char)) {
+      // if the char is a number/letter AND is a key in object, add one to count
+      // if the char is a number/letter AND not in object, add it to object and set value to 1
+      result[char] = ++result[char] || 1;
+    }
   }
   // return object at end
   return result
+}
+
+function isAlphaNumeric(char){
+  let code = char.charCodeAt(0);
+  if(!(code > 47 && code < 58) && // numeric (0-9)
+     !(code > 64 && code < 91) && // upper alpha (A-Z)
+     !(code > 96 && code < 123)) {  // lower alpha (a-z)
+    return false;
+  }
+  return true;
 }
 
 console.log(charCount('Hello'));
