1e7ae61afc136bcc85aabe154dd74befca7fdcfb
sulad <multisula@gmail.com>
2023-04-07 23:47:24 +0900

    vaildAnagram2

--- section05/validAnagram.js
+++ section05/validAnagram.js
@@ -1,21 +1,38 @@
+// function validAnagram(str1, str2){
+//   // add whatever parameters you deem necessary - good luck!
+//   obj1 = {}
+//   obj2 = {}
+//   if(str1.length !== str2.length) {
+//     return false
+//   }
+//   for(let c of str1){
+//     obj1[c] = ++obj1[c] || 1
+//   }
+//   for(let c of str2){
+//     obj2[c] = ++obj2[c] || 1
+//   }
+//   for(let key in obj1){
+//     if(!obj2[key]){
+//       return false;
+//     }
+//     if(obj1[key] !== obj2[key]){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
 function validAnagram(str1, str2){
   // add whatever parameters you deem necessary - good luck!
-  obj1 = {}
-  obj2 = {}
   if(str1.length !== str2.length) {
     return false
   }
+  obj1 = {}
   for(let c of str1){
     obj1[c] = ++obj1[c] || 1
   }
   for(let c of str2){
-    obj2[c] = ++obj2[c] || 1
-  }
-  for(let key in obj1){
-    if(!obj2[key]){
-      return false;
-    }
-    if(obj1[key] !== obj2[key]){
+    if(!obj1[c]--){
       return false;
     }
   }
