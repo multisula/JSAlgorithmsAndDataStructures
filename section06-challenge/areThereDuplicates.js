0a611e158fe4a9d5f3ce93a88272afc97c621002
multisula <multisula@gmail.com>
2023-04-12 00:12:58 +0900

    areThereDuplicates-Two pointers, OneLine

--- section06-challenge/areThereDuplicates.js
+++ section06-challenge/areThereDuplicates.js
@@ -20,16 +20,35 @@ Time - O(n log n)
 
 Space - O(1)
  */
-function areThereDuplicates(...args) {
-  // good luck. (supply any arguments you deem necessary.)
-  const obj1 = {}
-  for (const arg of args) {
-    obj1[arg] = ++obj1[arg] || 1
-  }
-  for (const key in obj1) {
-    if(obj1[key] > 1) return true
-  }
-  return false
+// function areThereDuplicates(...args) {
+//   // good luck. (supply any arguments you deem necessary.)
+//   const obj1 = {}
+//   for (const arg of args) {
+//     obj1[arg] = ++obj1[arg] || 1
+//   }
+//   for (const key in obj1) {
+//     if(obj1[key] > 1) return true
+//   }
+//   return false
+// }
+
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a,b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while(next < args.length){
+//     if(args[start] === args[next]){
+//       return true
+//     }
+//     start++
+//     next++
+//   }
+//   return false
+// }
+
+function areThereDuplicates() {
+  return new Set(arguments).size !== arguments.length;
 }
 
 // areThereDuplicates(1, 2, 3) // false
